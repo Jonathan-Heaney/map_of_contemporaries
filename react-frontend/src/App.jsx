e104ff2d98bc35d1683b638a4db882884a2b1e20
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-08 10:45:12 -0600

    Create app component

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,4 +1,20 @@
+import React, { useState } from 'react';
+import RandomPerson from './RandomPerson';
+import OverlapList from './OverlapList';
+import FameOverlapList from './FameOverlapList';
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [randomPerson, setRandomPerson] = useState(null);
+  const [overlapList, setOverlapList] = useState([]);
+  const [fameOverlapList, setFameOverlapList] = useState([]);
+
+  return (
+    <div>
+      <RandomPerson setRandomPerson={setRandomPerson}></RandomPerson>
+      <OverlapList people={overlapList}></OverlapList>
+      <FameOverlapList people={fameOverlapList}></FameOverlapList>
+    </div>
+  );
 }
+
 export default App;
