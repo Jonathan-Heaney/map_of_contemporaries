aba048e339a3348ba6ce01b00d1286a974334565
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-08 10:04:23 -0600

    Configure CORS

--- moc-backend/map_of_contemporaries/settings.py
+++ moc-backend/map_of_contemporaries/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'map_of_contemporaries.urls'
 
