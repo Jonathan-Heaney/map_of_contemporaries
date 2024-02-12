5a351d71914238341181a94ad237b9c0163a0b60
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-02-12 11:57:30 -0600

    Add title

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -39,6 +39,7 @@ function App() {
 
   return (
     <div className="Container">
+      <h1 className="title">Map of Contemporaries</h1>
       <RandomPerson
         fetchRandomPerson={fetchRandomPerson}
         person={randomPerson}
--- react-frontend/src/RandomPerson.jsx
+++ react-frontend/src/RandomPerson.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import PersonDetail from './PersonDetail';
+import React from 'react';
 
 function RandomPerson({ fetchRandomPerson, person }) {
   return (
