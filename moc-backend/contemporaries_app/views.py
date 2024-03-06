038326e161095713fec64e1038bbfd4e7b42302e
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-03-06 09:34:52 -0600

    Update views

--- moc-backend/contemporaries_app/views.py
+++ moc-backend/contemporaries_app/views.py
@@ -7,6 +7,8 @@ import random
 import urllib.parse
 
 # Function to generate Wikipedia links for famous people
+
+
 def generate_wikipedia_link(name):
     formatted_name = urllib.parse.quote(name.replace(" ", "_"))
     return f"https://en.wikipedia.org/wiki/{formatted_name}"
@@ -17,7 +19,8 @@ def random_person(request):
     # Get the minimum hpi from request parameters, default to 0 if not provided
     min_hpi = int(request.GET.get('min_hpi', 50))
 
-    valid_persons = FamousPerson.objects.exclude(birthyear__isnull=True).exclude(deathyear__isnull=True).filter(hpi__gte=min_hpi)
+    valid_persons = FamousPerson.objects.exclude(birthyear__isnull=True).exclude(
+        deathyear__isnull=True).filter(hpi__gte=min_hpi)
     person_count = valid_persons.count()
     random_index = random.randint(0, person_count - 1)
     person = valid_persons.all()[random_index]
@@ -33,16 +36,19 @@ def random_person(request):
     })
 
 # Helper function to calculate the overlap percentage
+
+
 def calculate_overlap_percentage(person1, person2):
     # Check for None values in birth and death years
     if None in (person1.birthyear, person1.deathyear, person2.birthyear, person2.deathyear):
         return 0
-    
+
     latest_start = max(person1.birthyear, person2.birthyear)
     earliest_end = min(person1.deathyear, person2.deathyear)
     overlap = max(0, earliest_end - latest_start)
     person1_lifespan = person1.deathyear - person1.birthyear
-    overlap_percentage = (overlap / person1_lifespan) * 100 if person1_lifespan > 0 else 0
+    overlap_percentage = (overlap / person1_lifespan) * \
+        100 if person1_lifespan > 0 else 0
     return round(overlap_percentage, 2)
 
 
@@ -53,10 +59,11 @@ def top_overlap(request, person_id):
 
     # Compare each person in the database to the randomly-chosen person
     for person in all_people:
-        overlap_percentage = calculate_overlap_percentage(chosen_person, person)
+        overlap_percentage = calculate_overlap_percentage(
+            chosen_person, person)
         overlaps.append((person, overlap_percentage))
         person.wikipedia_link = generate_wikipedia_link(person.name)
-    
+
     # Sort by overlap percentage and select top 10
     # x[1] is the second item in the tuple, which is the overlap percentage
     # reverse=True to sort the list in descending order,rather than the default which is ascending
@@ -86,7 +93,8 @@ def fame_overlap(request, person_id):
 
     # Compare each person in the database to the randomly-chosen person
     for person in all_people:
-        overlap_percentage = calculate_overlap_percentage(chosen_person, person)
+        overlap_percentage = calculate_overlap_percentage(
+            chosen_person, person)
         fame_overlap_score = overlap_percentage * (person.hpi ** 20)
         fame_overlaps.append((person, fame_overlap_score, overlap_percentage))
         person.wikipedia_link = generate_wikipedia_link(person.name)
