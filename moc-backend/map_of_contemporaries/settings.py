44037e5af732e3e9c373463f1532e6c67f91cb9f
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-03-18 12:54:28 -0500

    Configure RDS

--- moc-backend/map_of_contemporaries/settings.py
+++ moc-backend/map_of_contemporaries/settings.py
@@ -20,6 +20,8 @@ load_dotenv()
 DB_NAME = os.environ.get('DB_NAME')
 DB_USERNAME = os.environ.get('DB_USERNAME')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,8 +105,9 @@ DATABASES = {
         'NAME': DB_NAME,       # The name of your database
         'USER': DB_USERNAME,       # The username you created
         'PASSWORD': DB_PASSWORD,  # The password for the username
-        'HOST': 'localhost',      # Set to your database host
-        'PORT': '',               # Leave as an empty string to use the default port
+        # Set to your database host
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
     }
 }
 
